e385d56e7e412272b3cae884fcfbff74c74dbdcd
Anh <anhddo93@gmail.com>
2023-03-07 18:58:38 -0500

    update

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -18,3 +18,19 @@ require("luasnip").config.set_config({ -- Setting LuaSnip config
   -- Use Tab (or some other key if you prefer) to trigger visual selection
   store_selection_keys = "<Tab>",
 })
+
+-- Two ways to load snippets from both LuaSnip1 and LuaSnip2
+-- 1. Using a table
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
+-- 2. Using a comma-separated list
+-- Abbreviations used in this article and the LuaSnip docs
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
